e76714b8ac964602c6a3298039362c0c99d62641
Mike Watkins <github@mikewatkins.ca>
2024-02-03 19:50:23 -0800

    rename

--- private_dot_local/bin/executable_startdwl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Starts the dwl window manager
-Environment=wayland
-
-eval $(/usr/bin/gnome-keyring-daemon --start)
-export SSH_AUTH_SOCK
-export XKB_DEFAULT_OPTIONS=ctrl:nocaps
-pipewire &
-exec /usr/bin/dbus-launch /usr/bin/dwl -s /usr/bin/somebar
