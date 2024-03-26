40495a81f86ca982a595a694fc4fba01078dbf72
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-03-26 13:37:41 -0700

    add ip block

--- private_dot_local/bin/executable_dropips.sh
+++ private_dot_local/bin/executable_dropips.sh
@@ -18,9 +18,14 @@ for ip in $(grep "mox.*failed auth" /var/log/socklog/messages/* | awk '{print $1
 done
 # prolific spammers
 iptables -A INPUT -j DROP -s 194.169.175.0/24 # gb
-iptables -A INPUT -j DROP -s 41.211.128.0/19  # ga porn bitcoin scam
-iptables -A INPUT -j DROP -s 179.152.0.0/14   # br porn bitcoin scam
-iptables -A INPUT -j DROP -s 196.176.0.0/14   # tn bitcoin porn scam
+iptables -A INPUT -j DROP -s 194.169.175.0/24 # gb
+
+iptables -A INPUT -j DROP -s 41.211.128.0/19 # ga porn bitcoin scam
+iptables -A INPUT -j DROP -s 179.152.0.0/14  # br porn bitcoin scam
+iptables -A INPUT -j DROP -s 196.176.0.0/14  # tn bitcoin porn scam
 # spf and dmarc violators
 iptables -A INPUT -j DROP -s 191.180.0.0/14  # br
 iptables -A INPUT -j DROP -s 103.31.179.0/24 # bd
+
+# msedge.exe / exploit
+iptables -A INPUT -j DROP -s 212.102.40.0/23 # uk /dallas us
