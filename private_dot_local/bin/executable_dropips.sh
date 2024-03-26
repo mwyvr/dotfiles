87c91f5a2dbe5d73b724d9a88d8050b336753449
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-03-26 12:28:18 -0700

    ensure admin not DROPed

--- private_dot_local/bin/executable_dropips.sh
+++ private_dot_local/bin/executable_dropips.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # NOTE: Use countryblock.sh to block the worst offenders
 # Parses maillog, blocking auth violators and specific scam/spammers
+
+# don't block us/assumes only the sysadmin(s) or other authorized users have ssh access
+DONTBLOCK=$(ss | grep ssh | grep ESTAB | awk '{print $6}' | cut -d ":" -f 1 | sort | uniq)
 for ip in $(grep "mox.*failed auth" /var/log/socklog/messages/* | awk '{print $13}' | cut -d "=" -f 2 | sort | uniq); do
-	iptables -A INPUT -s $ip -j DROP
-	echo $ip
+	case $ip in
+	$DONTBLOCK)
+		iptables -I INPUT -s $ip -j ACCEPT
+		continue
+		;;
+	*)
+		iptables -A INPUT -s $ip -j DROP
+		echo $ip
+		;;
+	esac
 done
 # prolific spammers
 iptables -A INPUT -j DROP -s 194.169.175.0/24 # gb
