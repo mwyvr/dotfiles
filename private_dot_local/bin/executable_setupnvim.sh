d7cc8fa59ef4eb4139fe467ee45ec522e6e12162
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-12 13:55:57 -0700

    feat: setup utils for neovim

---
+++ private_dot_local/bin/executable_setupnvim.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+# install supports for neovim
+
+# not in chimera cports (yet), mapped to space-l
+go install github.com/jesseduffield/lazygit@latest
+
+. /etc/os-release
+if [ $ID = "chimera" ]; then
+	doas apk add go nodejs python-pip fd ripgrep unzip wget curl wl-clipboard
+elif [ $ID = "void" ]; then
+	sudo xbps-install -Su go nodejs python3-pip fd ripgrep unzip wget curl wl-clipboard
+else
+	echo "Unknown Linux distribution, terminating."
+	exit 1
+fi
