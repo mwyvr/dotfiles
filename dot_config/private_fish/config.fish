645fe10ebc4ca3a304263744a0c0dbd1cefa1502
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-12 10:23:01 -0700

    shorter title

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,8 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_key_bindings fish_vi_key_bindings
 end
-# function fish_title
-#     # `prompt_pwd` shortens the title. This helps prevent tabs from becoming very wide.
-#     echo $argv[1] (prompt_pwd)
-#     pwd
-# end
+function fish_title
+    # `prompt_pwd` shortens the title. This helps prevent tabs from becoming very wide.
+    echo $argv[1] (prompt_pwd)
+    pwd
+end
