6bb964563522b0f298bc85aa3df88bbde93c5b44
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-14 09:20:05 -0700

    fix: update repo

--- private_dot_local/bin/executable_setupnvim.sh
+++ private_dot_local/bin/executable_setupnvim.sh
@@ -6,6 +6,7 @@ go install github.com/jesseduffield/lazygit@latest
 
 . /etc/os-release
 if [ $ID = "chimera" ]; then
+	doas apk update
 	doas apk add go nodejs python-pip fd ripgrep unzip wget curl wl-clipboard
 elif [ $ID = "void" ]; then
 	sudo xbps-install -Su go nodejs python3-pip fd ripgrep unzip wget curl wl-clipboard
