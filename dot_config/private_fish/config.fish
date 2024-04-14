96a55fc3c244ec947811a75cdc7e990c64cd09ee
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-14 09:24:30 -0700

    path at end

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,13 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_key_bindings fish_vi_key_bindings
 end
-function fish_title
-    # `prompt_pwd` shortens the title. This helps prevent tabs from becoming very wide.
-    echo $argv[1] (prompt_pwd)
-    pwd
-end
 
-fish_add_path -m ~/.local/bin
+fish_add_path ~/.local/bin
 fish_add_path ~/go/bin
 
 set -U EDITOR nvim
