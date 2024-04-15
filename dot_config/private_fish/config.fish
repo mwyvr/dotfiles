76e2d41e97c994a26caca73f234546b449181989
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-15 10:39:42 -0700

    remove unused

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,12 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_key_bindings fish_vi_key_bindings
+    set -U EDITOR hx
 end
 
-fish_add_path -m ~/.local/bin
+fish_add_path ~/.local/bin
 fish_add_path ~/go/bin
-
-set -U EDITOR hx
-set -U LIBVIRT_DEFAULT_URI qemu:///system
-# for wlroots
-set -U XKB_DEFAULT_OPTIONS ctrl:nocaps
