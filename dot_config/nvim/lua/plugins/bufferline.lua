1970be262d2f749bbc8e70952c5dee6a0de4de21
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-18 12:00:09 -0700

    disable buffer tabs in nvim and helix

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,5 +1,7 @@
-return {'akinsho/bufferline.nvim', 
-  version = "*", 
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = true
-}
+return {}
+-- April 2024 disabling buffer tabs in neovim and helix in favour of telescope
+-- return {'akinsho/bufferline.nvim',
+--   version = "*",
+--   dependencies = 'nvim-tree/nvim-web-devicons',
+--   config = true
+-- }
