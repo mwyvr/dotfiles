c704394ab45f2973c08aa4ef54cbd797071190a1
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-18 12:26:12 -0700

    add cargo

--- private_dot_local/bin/executable_setupnvim.sh
+++ private_dot_local/bin/executable_setupnvim.sh
@@ -7,9 +7,9 @@ go install github.com/jesseduffield/lazygit@latest
 . /etc/os-release
 if [ $ID = "chimera" ]; then
 	doas apk update
-	doas apk add go nodejs python-pip fd ripgrep unzip wget curl wl-clipboard
+	doas apk add go nodejs python-pip cargo fd ripgrep unzip wget curl wl-clipboard
 elif [ $ID = "void" ]; then
-	sudo xbps-install -Su go nodejs python3-pip fd ripgrep unzip wget curl wl-clipboard
+	sudo xbps-install -Su go nodejs python3-pip cargo fd ripgrep unzip wget curl wl-clipboard
 else
 	echo "Unknown Linux distribution, terminating."
 	exit 1
