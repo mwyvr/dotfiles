4f0f113ae5d00fe4c8e6d646b8972ce19078a7bd
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-19 14:32:32 -0700

    use mini notify instead

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -88,10 +88,8 @@ return {
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
+			-- prefer mini notify, comment this out:
+			-- "rcarriga/nvim-notify",
 			config = function()
 				require("noice").setup({
 					lsp = {
