71c883bf6a97cc0e4e70b120adc927f576342978
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-19 12:02:04 -0700

    cleanup: consolidate git related

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,4 +1,21 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = true
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+		config = false,
+	},
 }
