0b113f59bce3639e7f1e510d48101c38146f2d44
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-19 14:30:59 -0700

    fix: keymap

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -27,56 +27,84 @@ return {
 			local null_ls = require("null-ls")
 			--	local map_lsp_keybinds = require("user.keymaps").map_lsp_keybinds -- Has to load keymaps before pluginslsp
 			local map_lsp_keybinds = function(buffer_number)
-				nnoremap("<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame", buffer = buffer_number })
-				nnoremap(
+				vim.keymap.set(
+					"n",
+					"<leader>rn",
+					vim.lsp.buf.rename,
+					{ desc = "LSP: [R]e[n]ame", buffer = buffer_number }
+				)
+				vim.keymap.set(
+					"n",
 					"<leader>ca",
 					vim.lsp.buf.code_action,
 					{ desc = "LSP: [C]ode [A]ction", buffer = buffer_number }
 				)
 
-				nnoremap("gd", vim.lsp.buf.definition, { desc = "LSP: [G]oto [D]efinition", buffer = buffer_number })
+				vim.keymap.set(
+					"n",
+					"gd",
+					vim.lsp.buf.definition,
+					{ desc = "LSP: [G]oto [D]efinition", buffer = buffer_number }
+				)
 
 				-- Telescope LSP keybinds --
-				nnoremap(
+				vim.keymap.set(
+					"n",
 					"gr",
 					require("telescope.builtin").lsp_references,
 					{ desc = "LSP: [G]oto [R]eferences", buffer = buffer_number }
 				)
 
-				nnoremap(
+				vim.keymap.set(
+					"n",
 					"gi",
 					require("telescope.builtin").lsp_implementations,
 					{ desc = "LSP: [G]oto [I]mplementation", buffer = buffer_number }
 				)
 
-				nnoremap(
+				vim.keymap.set(
+					"n",
 					"<leader>bs",
 					require("telescope.builtin").lsp_document_symbols,
 					{ desc = "LSP: [B]uffer [S]ymbols", buffer = buffer_number }
 				)
 
-				nnoremap(
+				vim.keymap.set(
+					"n",
 					"<leader>ps",
 					require("telescope.builtin").lsp_workspace_symbols,
 					{ desc = "LSP: [P]roject [S]ymbols", buffer = buffer_number }
 				)
 
 				-- See `:help K` for why this keymap
-				nnoremap("K", vim.lsp.buf.hover, { desc = "LSP: Hover Documentation", buffer = buffer_number })
-				nnoremap(
+				vim.keymap.set(
+					"n",
+					"K",
+					vim.lsp.buf.hover,
+					{ desc = "LSP: Hover Documentation", buffer = buffer_number }
+				)
+				vim.keymap.set(
+					"n",
 					"<leader>k",
 					vim.lsp.buf.signature_help,
 					{ desc = "LSP: Signature Documentation", buffer = buffer_number }
 				)
-				inoremap(
+				vim.keymap.set(
+					"i",
 					"<C-k>",
 					vim.lsp.buf.signature_help,
 					{ desc = "LSP: Signature Documentation", buffer = buffer_number }
 				)
 
 				-- Lesser used LSP functionality
-				nnoremap("gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration", buffer = buffer_number })
-				nnoremap(
+				vim.keymap.set(
+					"n",
+					"gD",
+					vim.lsp.buf.declaration,
+					{ desc = "LSP: [G]oto [D]eclaration", buffer = buffer_number }
+				)
+				vim.keymap.set(
+					"n",
 					"td",
 					vim.lsp.buf.type_definition,
 					{ desc = "LSP: [T]ype [D]efinition", buffer = buffer_number }
