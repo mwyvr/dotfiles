d496c67d950147b7414060d22c1869b6d344cad3
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-19 14:31:50 -0700

    add ensure_installed

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,46 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			highlight = { enable = true },
+			indent = { enable = true },
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"go",
+				"html",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"templ",
+				"toml",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+			},
+
+			config = function()
+				local config = require("nvim-treesitter.configs")
+				config.setup({
+					auto_install = true,
+					highlight = { enable = true },
+					indent = { enable = true },
+					opts = opts,
+				})
+			end,
+		},
+	},
 }
