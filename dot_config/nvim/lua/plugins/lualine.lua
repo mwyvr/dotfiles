35a1cbfc01cc0ab90ae07b20999e41c535f12e5e
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-19 14:54:06 -0700

    feat: add icons and neo-tree integration

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,32 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config=true,
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.g.lualine_laststatus = vim.o.laststatus
+		if vim.fn.argc(-1) > 0 then
+			-- set an empty statusline till lualine loads
+			vim.o.statusline = " "
+		else
+			-- hide the statusline on the starter page
+			vim.o.laststatus = 0
+		end
+	end,
+	opts = function()
+		-- PERF: we don't need this lualine require madness 🤷
+		local lualine_require = require("lualine_require")
+		lualine_require.require = require
+
+		local icons = require("util").icons
+
+		vim.o.laststatus = vim.g.lualine_laststatus
+
+		return {
+			options = {
+				theme = "auto",
+				globalstatus = true,
+				disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+			},
+			extensions = { "neo-tree", "lazy" },
+		}
+	end,
 }
