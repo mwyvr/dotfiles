1135fc61277bcf59a48a47644b723f324c3192fd
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-22 22:11:53 -0700

    feat: support opensuse

--- private_dot_local/bin/executable_setupnvim.sh
+++ private_dot_local/bin/executable_setupnvim.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/sh
 # install supports for neovim
 
-# not in chimera cports (yet), mapped to space-l
-go install github.com/jesseduffield/lazygit@latest
 
 . /etc/os-release
 if [ $ID = "chimera" ]; then
 	doas apk update
 	doas apk add go nodejs python-pip cargo fd ripgrep unzip wget curl wl-clipboard
+    # not in chimera cports (yet), mapped to space-l
+    go install github.com/jesseduffield/lazygit@latest
 elif [ $ID = "void" ]; then
 	sudo xbps-install -Su go nodejs python3-pip cargo fd ripgrep unzip wget curl wl-clipboard
+elif grep -q "opensuse" /etc/os-release ; then
+	sudo zypper in go1.22 nodejs python312-pip cargo fd ripgrep unzip wget curl wl-clipboard make
 else
 	echo "Unknown Linux distribution, terminating."
 	exit 1
