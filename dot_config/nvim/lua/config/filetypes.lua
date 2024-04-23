c752b2404f40ef417c473debf547452f1f11a6c3
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-23 08:20:04 -0700

    golang tmpl wip, not working

--- dot_config/nvim/lua/config/filetypes.lua
+++ dot_config/nvim/lua/config/filetypes.lua
@@ -1,3 +1,5 @@
 -- go templ template system
 -- https://templ.guide/commands-and-tools/ide-support/
 vim.filetype.add({ extension = { templ = "templ" } })
+-- for golang text/template html/template
+vim.filetype.add({ extension = { go = "tmpl" } })
