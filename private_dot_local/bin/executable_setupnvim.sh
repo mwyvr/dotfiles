88de220b7e4dcf07f0b8e9b9b0608a9e106f532d
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-23 08:36:12 -0700

    add: c/c++ for various

--- private_dot_local/bin/executable_setupnvim.sh
+++ private_dot_local/bin/executable_setupnvim.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/sh
-# install supports for neovim
-
-
+# install supports for neovim on various Linux distributions (Chimera Linux, Void Linux, openSUSE)
 . /etc/os-release
 if [ $ID = "chimera" ]; then
 	doas apk update
-	doas apk add go nodejs python-pip cargo fd ripgrep unzip wget curl wl-clipboard
+	doas apk add go nodejs python-pip cargo fd ripgrep unzip wget curl wl-clipboard clang cmake
     # not in chimera cports (yet), mapped to space-l
     go install github.com/jesseduffield/lazygit@latest
 elif [ $ID = "void" ]; then
-	sudo xbps-install -Su go nodejs python3-pip cargo fd ripgrep unzip wget curl wl-clipboard
-elif grep -q "opensuse" /etc/os-release ; then
-	sudo zypper in go1.22 nodejs python312-pip cargo fd ripgrep unzip wget curl wl-clipboard make
+	sudo xbps-install -Su go nodejs python3-pip cargo fd ripgrep unzip wget curl wl-clipboard base-devel
+elif [ $ID = "opensuse-tumbleweed" ]; then
+	sudo zypper in go1.22 nodejs python312-pip cargo fd ripgrep unzip wget curl wl-clipboard gcc gcc-c++ make
+elif [ $ID = "opensuse-aeon" ]; then
+    echo "This system runs immutable openSUSE Aeon; run this script in a container."
 else
 	echo "Unknown Linux distribution, terminating."
 	exit 1
