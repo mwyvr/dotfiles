12d9fd79d283abce735d792b6c7a4b085a803b09
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-24 08:14:40 -0700

    feat: ctrl-f accepts auto suggestion

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,14 @@
 if status is-interactive
     set -g fish_key_bindings fish_vi_key_bindings
     set -U EDITOR nvim
+    set fish_greeting # disable welcome
+end
+
+# make ctrl-f accept auto suggestion in vi mode as it does in emacs mode
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
 end
 
 fish_add_path ~/.local/bin
