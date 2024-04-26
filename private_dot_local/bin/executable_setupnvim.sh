e0a658230b4a10572a40202f016f48dc0c153c24
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-26 09:44:35 -0700

    autofmt

--- private_dot_local/bin/executable_setupnvim.sh
+++ private_dot_local/bin/executable_setupnvim.sh
@@ -2,28 +2,33 @@
 # install supports for neovim on various Linux distributions (Chimera Linux, Void Linux, openSUSE Tumbleweed/Aeon in a tw container)
 . /etc/os-release
 case $ID in
-    chimera)
-        doas apk update
-        doas apk add neovim tmux go nodejs python-pip cargo fd ripgrep unzip wget curl wl-clipboard clang cmake
-        # not in chimera cports (yet), mapped to space-l
-        go install github.com/jesseduffield/lazygit@latest
-        ;;
-    void)
-	    sudo xbps-install -Su neovim tmux go lazygit nodejs python3-pip cargo fd ripgrep unzip wget curl wl-clipboard base-devel
-        ;;
-    "opensuse-tumbleweed")
-        sudo zypper refresh
-	    sudo zypper in neovim tmux go1.22 lazygit nodejs python312-pip cargo fd ripgrep unzip wget curl wl-clipboard gcc gcc-c++ make awk
-        ;;
-    "opensuse-aeon")
-        echo "This system runs immutable openSUSE Aeon; run this script in a Tumbleweed container."
-        ;;
-    "opensuse-microos")
-        echo "This system runs immutable openSUSE Aeon; run this script in a Tumbleweed container."
-        ;;
-    *)
-        echo "Unknown Linux distribution, terminating."
-        exit 1
-        ;;
+chimera)
+	doas apk update
+	doas apk add neovim tmux go git nodejs python-pip cargo fd ripgrep unzip wget curl wl-clipboard clang cmake awk
+	# not in chimera cports (yet)
+	go install github.com/jesseduffield/lazygit@latest
+	;;
+void)
+	sudo xbps-install -Su neovim tmux go lazygit nodejs python3-pip cargo fd ripgrep unzip wget curl wl-clipboard base-devel awk
+	;;
+"opensuse-tumbleweed")
+	sudo zypper refresh
+	sudo zypper in neovim tmux go1.22 git lazygit nodejs python312-pip cargo fd ripgrep unzip wget curl wl-clipboard gcc gcc-c++ make awk
+	;;
+"opensuse-aeon")
+	echo "This system runs immutable openSUSE Aeon; run this script in the default Tumbleweed container."
+	echo "e.g.:"
+	echo "    distrobox enter"
+	echo "    setupnvim.sh"
+	;;
+"opensuse-microos")
+	echo "This system runs immutable openSUSE MicroOS (Aeon); run this script in the default Tumbleweed container."
+	echo "e.g.:"
+	echo "    distrobox enter"
+	echo "    setupnvim.sh"
+	;;
+*)
+	echo "Unknown Linux distribution; currently supports Chimera/Void/openSUSE Tumbleweed+Aeon. Terminating."
+	exit 1
+	;;
 esac
-
