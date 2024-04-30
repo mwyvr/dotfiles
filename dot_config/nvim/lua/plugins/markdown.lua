581f261ce92f820c98c28d2e00b8b0c47054a41b
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-30 09:46:30 -0700

    make lazy

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,9 +1,28 @@
 return {
+  -- Minimum config required
   -- LazyExtras includes these plus linter; linter makes many docs too busy
+  -- https://www.lazyvim.org/extras/lang/markdown
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      -- vim.list_extend(opts.ensure_installed, { "markdownlint", "marksman" })
+      vim.list_extend(opts.ensure_installed, { "marksman" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {},
+      },
+    },
+  },
   -- headlines in markdown documents
   {
     "lukas-reineke/headlines.nvim",
     dependencies = "nvim-treesitter/nvim-treesitter",
+    lazy = true,
     config = true, -- or `opts = {}`
   },
   {
