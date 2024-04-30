c1391dda6fd178f6d9584ec3734ab1c488e849dc
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-30 08:21:23 -0700

    add markdown headlines without linter

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,10 @@
 return {
   -- disable indent scope line *animation*
   { "echasnovski/mini.indentscope", enabled = false },
+  -- headlines in markdown documents; available in markdown extras but I don't like the linter
+  {
+    "lukas-reineke/headlines.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true, -- or `opts = {}`
+  },
 }
