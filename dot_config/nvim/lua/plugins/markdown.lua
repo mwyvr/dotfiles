f866e6234f1110576df9c3588fc8302a7fc2d1d3
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-04-30 08:35:21 -0700

    mv markdown to own plugin

---
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,28 @@
+return {
+  -- LazyExtras includes these plus linter; linter makes many docs too busy
+  -- headlines in markdown documents
+  {
+    "lukas-reineke/headlines.nvim",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true, -- or `opts = {}`
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
+  -- TODO: add a toggle to include linter
+}
