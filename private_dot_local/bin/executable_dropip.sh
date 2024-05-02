d61cb1a01fcb31f2c626a22323d6d62a3b6c3872
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-05-02 08:38:25 -0700

    feat: add logging and dontblock us

--- private_dot_local/bin/executable_dropip.sh
+++ private_dot_local/bin/executable_dropip.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-# Adds an ip or /netblock to iptables drop list, if it isn't already in there
-iptables -C INPUT -j DROP -s "$ip" >/dev/null 2>&1 || iptables -A INPUT -j DROP -s "$ip"
+if [ $# -eq 0 ]; then
+	echo "dropip.sh: error, no ip or ip block supplied as argument"
+fi
+ip=$1
+
+# don't block us/assumes only the sysadmin(s) or other authorized users have ssh access
+DONTBLOCK=$(ss | grep ssh | grep ESTAB | awk '{print $6}' | cut -d ":" -f 1 | sort | uniq)
+
+case "$ip" in
+$DONTBLOCK)
+	iptables -C INPUT -j ACCEPT -s "$ip" >/dev/null 2>&1 || iptables -I INPUT -s "$ip" -j ACCEPT
+	;;
+*)
+	# Adds an ip or /netblock to iptables drop list, if it isn't already in there
+	if ! iptables -C INPUT -j DROP -s "$ip" >/dev/null 2>&1; then
+		iptables -A INPUT -j DROP -s "$ip"
+		logger "dropip.sh DROP $ip"
+	fi
+	;;
+esac
