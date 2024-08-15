5bd33f47a09f0491ec74a2fe3ff7a094c36b7238
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-08-14 20:57:48 -0700

    clean up chimera entry

--- private_dot_local/bin/executable_setupnvim.sh
+++ private_dot_local/bin/executable_setupnvim.sh
@@ -5,40 +5,40 @@
 
 case $ID in
 chimera)
-	doas apk update
-	doas apk add neovim helix go git nodejs python-pip cargo fd ripgrep unzip wget curl wl-clipboard clang cmake awk
-	# not in chimera cports (yet)
-	go install github.com/jesseduffield/lazygit@latest
-	;;
+    doas apk update
+    doas apk add neovim helix fonts-nerd-roboto-mono go git nodejs python-pip cargo fd ripgrep unzip wget curl wl-clipboard clang cmake
+    # not in chimera cports (yet)
+    go install github.com/jesseduffield/lazygit@latest
+    ;;
 void)
-	if [ -z "$DISPLAY" ]; then
-		# probably a server
-		sudo xbps-install -Su neovim helix go lazygit nodejs python3-pip cargo fd ripgrep unzip wget curl base-devel
-	else
-		sudo xbps-install -Su neovim helix go lazygit nodejs python3-pip cargo fd ripgrep unzip wget curl base-devel wl-clipboard
-	fi
-	;;
+    if [ -z "$DISPLAY" ]; then
+        # probably a server
+        sudo xbps-install -Su neovim helix go lazygit nodejs python3-pip cargo fd ripgrep unzip wget curl base-devel
+    else
+        sudo xbps-install -Su neovim helix go lazygit nodejs python3-pip cargo fd ripgrep unzip wget curl base-devel wl-clipboard
+    fi
+    ;;
 "opensuse-tumbleweed")
-	sudo zypper refresh
-	sudo zypper in neovim helix go1.22 git lazygit nodejs python312-pip cargo fd ripgrep unzip wget curl wl-clipboard gcc gcc-c++ make
-	if [ $CONTAINER_ID == "tumbleweed" ]; then
-		distrobox-export --bin /usr/bin/nvim
-	fi
-	;;
-"opensuse-aeon")
-	echo "This system runs immutable openSUSE Aeon; run this script in the default Tumbleweed container."
-	echo "e.g.:"
-	echo "    distrobox enter"
-	echo "    setupnvim.sh"
-	;;
+    sudo zypper refresh
+    sudo zypper in neovim helix go1.22 git lazygit nodejs python312-pip cargo fd ripgrep unzip wget curl wl-clipboard gcc gcc-c++ make
+    if [ $CONTAINER_ID == "tumbleweed" ]; then
+        distrobox-export --bin /usr/bin/nvim
+    fi
+    ;;
+"aeon")
+    echo "This system runs immutable openSUSE Aeon; run this script in the default Tumbleweed container."
+    echo "e.g.:"
+    echo "    distrobox enter"
+    echo "    setupnvim.sh"
+    ;;
 "opensuse-microos")
-	echo "This system runs immutable openSUSE MicroOS (Aeon); run this script in the default Tumbleweed container."
-	echo "e.g.:"
-	echo "    distrobox enter"
-	echo "    setupnvim.sh"
-	;;
+    echo "This system runs immutable openSUSE MicroOS (Aeon); run this script in the default Tumbleweed container."
+    echo "e.g.:"
+    echo "    distrobox enter"
+    echo "    setupnvim.sh"
+    ;;
 *)
-	echo "Unknown Linux distribution; currently supports Chimera/Void/openSUSE Tumbleweed+Aeon. Terminating."
-	exit 1
-	;;
+    echo "Unknown Linux distribution; currently supports Chimera/Void/openSUSE Tumbleweed+Aeon. Terminating."
+    exit 1
+    ;;
 esac
