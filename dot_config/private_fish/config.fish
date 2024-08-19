f6dd457ad5f5c7fd5c7f5eb74f0b5d8477e8d1b6
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-08-19 16:39:44 -0700

    add qemu config var

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     set -g fish_key_bindings fish_vi_key_bindings
     set -U EDITOR nvim
     set fish_greeting # disable welcome
+    set -U LIBVIRT_DEFAULT_URI 'qemu:///system'
 end
 
 # make ctrl-f accept auto suggestion in vi mode as it does in emacs mode
