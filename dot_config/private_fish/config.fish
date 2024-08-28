47768495a206b35b80136acc78ed78fac7e7bf53
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-08-28 10:35:47 -0700

    export EDITOR variable

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
     set -g fish_key_bindings fish_vi_key_bindings
-    set -U EDITOR hx
+    set -x EDITOR hx
     set fish_greeting # disable welcome
-    set -U LIBVIRT_DEFAULT_URI 'qemu:///system'
+    set -x LIBVIRT_DEFAULT_URI 'qemu:///system'
 end
 
 # make ctrl-f accept auto suggestion in vi mode as it does in emacs mode
