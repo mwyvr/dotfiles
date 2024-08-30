98dd63eaf6360985f42442590831f93e6df76593
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-08-30 15:01:54 -0700

    make generic for laptop or desktop use

--- private_dot_local/bin/executable_chimera-configure.sh
+++ private_dot_local/bin/executable_chimera-configure.sh
@@ -1,34 +1,60 @@
 #!/bin/sh
-export USER=mw
+echo "Configure Chimera Linux for laptop or desktop (implying KVM host)"
+echo "Press a key to continue or ctrl-c to abort"
+read -r FOO
+unset $FOO
+# Adds necessary packages and services.
+
 doas apk update
 doas apk upgrade
-doas apk add gnome-tweaks git helix neovim wl-copy go chezmoi foot fish-shell fonts-nerd-roboto-mono htop
-# lazygit not yet in cports
-go install github.com/jesseduffield/lazygit@latest
 doas apk add chrony
 doas dinitctl enable chrony
 doas dinitctl enable syslog-ng
-doas dinitctl enable sshd
-doas apk add ucode-intel
-doas update-initramfs -c -k all
-# ensure machine does not suspend at gdm login prompt
-doas -u _gdm dbus-run-session gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
-doas -u _gdm dbus-run-session gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 
-# containers
+if [ ! -d "/proc/acpi/button/lid" ]; then
+    # machine is a desktop
+    doas dinitctl enable sshd
+    # ensure machine does not suspend at gdm login prompt; may need to configure /etc/elogind
+    doas -u _gdm dbus-run-session gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+    doas -u _gdm dbus-run-session gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+    # kvm/qemu virtual machine support
+    doas apk add qemu qemu-edk2-firmware qemu-system-x86_64 libvirt virt-manager virt-viewer ufw
+    doas usermod -aG kvm libvirt $USER
+    # virtlockd and virtlogd are enabled automatically by the following
+    for svc in virtqemud virtnodedevd virtstoraged virtnetworkd; do
+        dinitctl enable $svc
+    done
+else
+    # machine is a laptop
+    doas apk add powertop
+fi
+
+# cpu microcode
+if lscpu | grep "GenuineIntel"; then
+    doas apk add ucode-intel
+else
+    doas apk add ucode-amd
+fi
+
+# after microcode
+doas update-initramfs -c -k all
+
+# flatpak apps
 doas add flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-# more containers
-doas apk add qemu qemu-edk2-firmware qemu-system-x86_64 libvirt virt-manager virt-viewer ufw
-doas usermod -aG kvm libvirt $USER
-# virtlockd and virtlogd are enabled automatically by the following
-for svc in virtqemud virtnodedevd virtstoraged virtnetworkd; do
-    dinitctl enable $svc
+for app in com.google.Chrome org.signal.Signal us.zoom.Zoom; do
+    echo "Installing $app"
+    flatpak install -y $app
 done
+
+# core utils
+doas apk add gnome-tweaks git chezmoi foot fish-shell fonts-nerd-roboto-mono htop
+
+# for Helix & dev
+doas apk add helix go nodejs
+
+# lazygit not yet in cports
+go install github.com/jesseduffield/lazygit@latest
 
 # applications
 doas apk add evolution
-
-# fix gdm so it doesn't suspend on this desktop, seems to be the default now but was not
-doas -u _gdm dbus-run-session gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing
