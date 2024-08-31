84591ae63cb096cb84b4ef3e960605e7e447eba7
Mike Watkins <4117918+mwyvr@users.noreply.github.com>
2024-08-31 12:08:54 -0700

    superfluous

--- private_dot_local/bin/executable_setup-base-distrobox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Sets up an already existing "twbox" with basic cli applications
-sudo zypper -n install helix go chezmoi git nodejs whois
-distrobox-export --bin /usr/bin/helix
-distrobox-host-exec ln -svf ~/.local/bin/helix ~/.local/bin/hx
-distrobox-export --bin /usr/bin/chezmoi
-distrobox-export --bin /usr/bin/git
