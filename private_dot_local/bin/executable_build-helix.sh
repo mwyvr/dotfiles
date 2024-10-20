5633534beebe263e95385e86dd618d98f1f53e56
Mike Watkins <git@mikewatkins.ca>
2024-10-20 16:05:29 -0700

    forgot to pull

--- private_dot_local/bin/executable_build-helix.sh
+++ private_dot_local/bin/executable_build-helix.sh
@@ -5,5 +5,6 @@ if [ ! -d helix ]; then
     git clone https://github.com/helix-editor/helix.git
 fi
 cd helix
+git pull
 RUSTFLAGS="-C target-feature=-crt-static" cargo install --path helix-term --locked
 ln -Tsv ~/src/helix/runtime ~/.config/helix/runtime
