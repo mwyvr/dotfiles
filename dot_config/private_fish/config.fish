96092dbf5cdfe5965c5b1c5d3a21fce7ff9d68c6
Mike Watkins <git@mikewatkins.ca>
2024-10-24 12:29:46 -0700

    reorg

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,20 +1,22 @@
+if status is-login
+    set -gx HOSTNAME (hostname)
+    set -gx LIBVIRT_DEFAULT_URI 'qemu:///system'
+    set -gx EDITOR hx
+    fish_add_path ~/.cargo/bin
+    fish_add_path ~/go/bin
+    fish_add_path ~/.local/bin
+    envsource ~/.env
+end
+
 if status is-interactive
     set -g fish_key_bindings fish_vi_key_bindings
-    set -x EDITOR hx
-    set -x HOSTNAME (hostname)
-    set fish_greeting # disable welcome
-    set -x LIBVIRT_DEFAULT_URI 'qemu:///system'
+    set -g fish_greeting # disable welcome
 end
 
 # make ctrl-f accept auto suggestion in vi mode as it does in emacs mode
+# this has to be in config.fish for some reason
 function fish_user_key_bindings
     for mode in insert default visual
         bind -M $mode \cf forward-char
     end
 end
-
-fish_add_path ~/.cargo/bin
-fish_add_path ~/go/bin
-fish_add_path ~/.local/bin
-
-envsource ~/.env
