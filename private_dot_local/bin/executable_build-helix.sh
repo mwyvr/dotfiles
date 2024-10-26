56741ddf60f3200d48c6242f6f5acd71d9c6bb2a
Mike Watkins <git@mikewatkins.ca>
2024-10-25 22:38:01 -0700

    fixes for Aeon

--- private_dot_local/bin/executable_build-helix.sh
+++ private_dot_local/bin/executable_build-helix.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+. /etc/os-release
+
+case $ID in
+"opensuse-tumbleweed")
+    # be sure we're in the right distrobox container
+    if [ "$CONTAINER_ID" != "tumbleweed" ]; then
+        echo "run this in the default tumbleweed container"
+        exit 1
+    fi
+    ;;
+"aeon")
+    echo "On Aeon - run this within the default tumbleweed distrobox. Terminating."
+    exit 1
+    ;;
+*)
+    echo "Unknown Linux distribution, terminating."
+    exit 1
+    ;;
+esac
+
 mkdir -p ~/src
 cd ~/src
 if [ ! -d helix ]; then
@@ -6,5 +26,15 @@ if [ ! -d helix ]; then
 fi
 cd helix
 git pull
+
+# build it
 RUSTFLAGS="-C target-feature=-crt-static" cargo install --path helix-term --locked
-ln -Tsv ~/src/helix/runtime ~/.config/helix/runtime
+
+# if on aeon/in a container
+if [ "$CONTAINER_ID" = "tumbleweed" ]; then
+    echo "Moving hx to /usr/bin"
+    sudo mv ~/.cargo/bin/hx /usr/bin/hx
+    distrobox-export --bin /usr/bin/hx
+fi
+
+ln -Tsvf ~/src/helix/runtime ~/.config/helix/runtime
