bddf45d153d0c1517e8e6fa2841810a1604df636
Mike Watkins <git@mikewatkins.ca>
2024-11-28 11:29:05 -0800

    new: finally automate adding bad actors to firewall

---
+++ private_dot_local/bin/executable_addtospammers.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Appends valid ip addresses or netblocks to spammers table for firewall actions
+SPAMMERS="/usr/local/etc/pftables/spammers"
+
+addtotable() {
+    if grep "$1" "$SPAMMERS"; then
+        echo "$0: $1 already in table: $SPAMMERS"
+    else
+        # add to running
+        pfctl -t spammers -T add "$1"
+        # add to our persistent list
+        echo "$1" | tee -a "$SPAMMERS"
+        echo "$0: added $1 to table: $SPAMMERS"
+    fi
+    exit
+}
+
+if ! [ $(id -u) -eq 0 ]; then
+    echo "$0: run as root"
+    exit 1
+fi
+
+if ! [ "$1" ]; then
+    echo "$0: no IP address/block provided"
+    exit 1
+fi
+
+# netblock supplied, add it
+if echo "$1" | grep -q -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}'; then
+    addtotable "$1"
+fi
+
+# no netblock supplied, check whois
+if echo "$1" | grep -q -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$'; then
+    whois "$1"
+    echo "$0: $1 was NOT added to spammers table; check whois info for a netblock instead."
+    exit
+fi
+
+# fallthrough
+echo "$0: $1 was NOT added to spammers table; is it valid?"
+exit 1
