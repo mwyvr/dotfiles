f6d5747d1502aea2c6aa76568d90b298bb1820d4
Mike Watkins <git@mikewatkins.ca>
2024-11-30 16:38:41 -0800

    fix: on linux, build in a distrobox

--- private_dot_local/bin/executable_knockknock.sh
+++ private_dot_local/bin/executable_knockknock.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 KNOCKR="$HOME/go/bin/knockr"
+
+. /etc/os-release
+
 if [ ! -x "$KNOCKR" ]; then
-	go install github.com/mwyvr/knockr@latest
+    case $ID in
+    "opensuse-tumbleweed")
+        # dev tools in a distrobox
+        if [ -z "${CONTAINER_ID}" ]; then
+            exec "/bin/distrobox-enter" -n tumbleweed -- '/bin/go install github.com/mwyvr/knockr@latest'
+        else
+            go install github.com/mwyvr/knockr@latest
+        fi
+        ;;
+    *)
+        go install github.com/mwyvr/knockr@latest
+        ;;
+    esac
 fi
 # port knock router to open wireguard
 . ~/.knock.env
