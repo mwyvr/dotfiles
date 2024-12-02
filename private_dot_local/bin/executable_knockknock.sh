caf11ab03e11b98093f5fb288ec6c54b9156fbd1
Mike Watkins <git@mikewatkins.ca>
2024-12-02 08:51:16 -0800

    fix: exec stopping at container build

--- private_dot_local/bin/executable_knockknock.sh
+++ private_dot_local/bin/executable_knockknock.sh
@@ -3,15 +3,22 @@ KNOCKR="$HOME/go/bin/knockr"
 
 . /etc/os-release
 
+distrobox_build() {
+    # if not in a container, enter it
+    if [ -z "${CONTAINER_ID}" ]; then
+        distrobox-enter -n tumbleweed -- /bin/go install github.com/mwyvr/knockr@latest
+    else
+        go install github.com/mwyvr/knockr@latest
+    fi
+}
+
 if [ ! -x "$KNOCKR" ]; then
     case $ID in
     "opensuse-tumbleweed")
-        # dev tools in a distrobox
-        if [ -z "${CONTAINER_ID}" ]; then
-            exec "/bin/distrobox-enter" -n tumbleweed -- '/bin/go install github.com/mwyvr/knockr@latest'
-        else
-            go install github.com/mwyvr/knockr@latest
-        fi
+        distrobox_build
+        ;;
+    "aeon")
+        distrobox_build
         ;;
     *)
         go install github.com/mwyvr/knockr@latest
