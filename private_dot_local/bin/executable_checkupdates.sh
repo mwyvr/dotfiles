3aba2c362cc9b4b8ca072fc6461316484325aac6
Mike Watkins <git@mikewatkins.ca>
2024-12-04 10:16:35 -0800

    add chimera support

--- private_dot_local/bin/executable_checkupdates.sh
+++ private_dot_local/bin/executable_checkupdates.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 . /etc/os-release
 
-UPDATES="system updates are available for $NAME."
+NUM=""
 
 case $ID in
 freebsd)
     doas pkg update >/dev/null
-    NUM=$(doas pkg upgrade --dry-run | grep -Eo "The following ([0-9].) package\(s\) will be affected" | cut -d ' ' -f 3)
-    if ! [ "$NUM" = "" ]; then
-        notify-send -c critical "$NUM $UPDATES"
-    fi
+    NUM=$(doas pkg upgrade --dry-run | grep -Eo "The following ([0-9]+) package\(s\) will be affected" | cut -d ' ' -f 3)
     ;;
 "opensuse-tumbleweed")
     sudo zypper refresh >/dev/null
-    NUM=$(sudo zypper --non-interactive dup --dry-run | grep -Eo "The following ([0-9].) package" | cut -d ' ' -f 3)
-    if ! [ "$NUM" = "" ]; then
-        notify-send -c critical "$NUM $UPDATES"
-    fi
+    NUM=$(sudo zypper --non-interactive dup --dry-run | grep -Eo "The following ([0-9]+) package" | cut -d ' ' -f 3)
+    ;;
+"chimera")
+    doas apk update -q >/dev/null
+    NUM=$(doas apk upgrade -s | grep -E -c "([0-9]+/[0-9]+)")
+    ;;
+*)
+    notify-send --transient "$0: unsupported $NAME"
+    exit
     ;;
 esac
+
+if ! [ "$NUM" = "" ]; then
+    notify-send --transient "$NUM system updates are available for $NAME"
+fi
