20ebcfb23991a318fc9e1e3873fe1f21eabf13fa
Mike Watkins <git@mikewatkins.ca>
2024-12-04 09:30:58 -0800

    improve output

--- private_dot_local/bin/executable_checkupdates.sh
+++ private_dot_local/bin/executable_checkupdates.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
-
 . /etc/os-release
-UPDATES="Updated $NAME packages available"
+
+UPDATES="system updates are available for $NAME."
 
 case $ID in
 freebsd)
     doas pkg update >/dev/null
-    if ! doas pkg upgrade --dry-run | grep -qi "up to date"; then
-        notify-send -c critical "$UPDATES"
+    NUM=$(doas pkg upgrade --dry-run | grep -Eo "The following ([0-9].) package\(s\) will be affected" | cut -d ' ' -f 3)
+    if ! [ "$NUM" == "" ]; then
+        notify-send -c critical "$NUM $UPDATES"
     fi
     ;;
 "opensuse-tumbleweed")
     sudo zypper refresh >/dev/null
-    if ! sudo zypper dup --dry-run | grep -qi "Nothing to do"; then
-        notify-send -c critical "$UPDATES"
+    NUM=$(sudo zypper dup --dry-run | grep -Eo "The following ([0-9].) package" | cut -d ' ' -f 3)
+    if ! [ "$NUM" == "" ]; then
+    if ! sudo zypper --non-interactive dup --dry-run | grep -qiv "Nothing to do"; then
+        notify-send -c critical "$NUM $UPDATES"
     fi
     ;;
 esac
