260fa5815b71a652c077ca6c7eaf34d90338aece
Mike Watkins <git@mikewatkins.ca>
2024-12-04 09:33:38 -0800

    oops

--- private_dot_local/bin/executable_checkupdates.sh
+++ private_dot_local/bin/executable_checkupdates.sh
@@ -7,15 +7,14 @@ case $ID in
 freebsd)
     doas pkg update >/dev/null
     NUM=$(doas pkg upgrade --dry-run | grep -Eo "The following ([0-9].) package\(s\) will be affected" | cut -d ' ' -f 3)
-    if ! [ "$NUM" == "" ]; then
+    if ! [ "$NUM" = "" ]; then
         notify-send -c critical "$NUM $UPDATES"
     fi
     ;;
 "opensuse-tumbleweed")
     sudo zypper refresh >/dev/null
     NUM=$(sudo zypper dup --dry-run | grep -Eo "The following ([0-9].) package" | cut -d ' ' -f 3)
-    if ! [ "$NUM" == "" ]; then
-    if ! sudo zypper --non-interactive dup --dry-run | grep -qiv "Nothing to do"; then
+    if ! [ "$NUM" = "" ]; then
         notify-send -c critical "$NUM $UPDATES"
     fi
     ;;
