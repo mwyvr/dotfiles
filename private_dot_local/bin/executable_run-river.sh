b6f4211907d7ac213eaf9428867b177bd5c8d1c7
Mike Watkins <git@mikewatkins.ca>
2025-01-06 19:04:52 +0000

    fix; missing var

--- private_dot_local/bin/executable_run-river.sh
+++ private_dot_local/bin/executable_run-river.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # called by greetd on linux
+WM=river
 export XDG_SESSION_TYPE=wayland
 export XDG_SESSION_DESKTOP="$WM"
 export XDG_CURRENT_DESKTOP="$WM"
@@ -8,8 +9,8 @@ export MOZ_ENABLE_WAYLAND=1
 export XKB_DEFAULT_OPTIONS=ctrl:nocaps
 
 # yes, this is intentional
-xdg-user-dirs-gtk-update
-xdg-user-dirs-gtk-update --force
+# xdg-user-dirs-gtk-update
+# xdg-user-dirs-gtk-update --force
 
 eval "$(gnome-keyring-daemon --start)"
 export SSH_AUTH_SOCK
