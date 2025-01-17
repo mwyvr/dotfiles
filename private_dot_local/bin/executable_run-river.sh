a37ae3aff9353720ced23a14a0752393b140bb79
Mike Watkins <git@mikewatkins.ca>
2025-01-16 20:19:12 -0800

    force dark theme

--- private_dot_local/bin/executable_run-river.sh
+++ private_dot_local/bin/executable_run-river.sh
@@ -7,6 +7,7 @@ export XDG_CURRENT_DESKTOP="$WM"
 export XDG_CONFIG_HOME="$HOME/.config"
 export MOZ_ENABLE_WAYLAND=1
 export XKB_DEFAULT_OPTIONS=ctrl:nocaps
+export GTK_THEME=Adwaita:dark
 
 eval "$(gnome-keyring-daemon --start)"
 export SSH_AUTH_SOCK
