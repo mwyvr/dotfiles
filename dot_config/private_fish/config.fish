89daa7a74d398a9cd00eca0ad5a204daa3872d32
Mike Watkins <git@mikewatkins.ca>
2025-02-10 10:13:03 -0800

    do away with is_login/is_interactive

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,21 +1,19 @@
-if status is-login
-    set -gx HOSTNAME (hostname)
-    envsource $HOME/.env
-    fish_add_path ~/.cargo/bin
-    fish_add_path ~/go/bin
-    fish_add_path ~/.local/bin
-    if type -q hx
-        set -gx EDITOR hx
-    end
-end
+# fish config
+fish_config theme choose mui-dark
+set -g fish_greeting # disable welcome
+set -g fish_key_bindings fish_vi_key_bindings
 
-if status is-interactive
-    fish_config theme choose mui-dark
-    set -gx XKB_DEFAULT_OPTIONS ctrl:nocaps
-    set -gx GTK_THEME Adwaita:dark
-    set -g fish_key_bindings fish_vi_key_bindings
-    set -g fish_greeting # disable welcome
+# env
+set -gx HOSTNAME (hostname)
+if type -q hx
+    set -gx EDITOR hx
 end
+fish_add_path ~/.cargo/bin
+fish_add_path ~/go/bin
+fish_add_path ~/.local/bin
+set -gx XKB_DEFAULT_OPTIONS ctrl:nocaps
+set -gx GTK_THEME Adwaita:dark
+envsource $HOME/.env
 
 # make ctrl-f accept auto suggestion in vi mode as it does in emacs mode
 # this has to be in config.fish for some reason
