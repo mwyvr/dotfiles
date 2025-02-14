ce46ee47d95806f96039d6e4d2a5d501b4804e39
Mike Watkins <git@mikewatkins.ca>
2025-02-14 21:34:17 +0000

    kbd bug

--- private_dot_local/bin/executable_configure-void.sh
+++ private_dot_local/bin/executable_configure-void.sh
@@ -37,8 +37,7 @@ EOF
     fi
     # my Varmilo keyboard firmware IDs as an Apple; the function keys act as
     # media keys unless pressed with Fn button, which is annoying.
-    VARMILO=$(dmesg | grep Varmilo)
-    if [ -z "$VARMILO" ]; then
+    if $DOAS dmesg | grep Varmilo; then
         echo "- Forcing Varmilo/hid_apple function keys as default (2)"
         # real time temp fix
         echo 2 | $DOAS tee -a /sys/module/hid_apple/parameters/fnmode
@@ -46,8 +45,7 @@ EOF
         echo "options hid_apple fnmode=2" | $DOAS tee -a /etc/modprobe.d/$(hostname).conf
     fi
 
-    ATHWIFI=$(lsmod | grep ath12k)
-    if [ -n "$ATHWIFI" ]; then
+    if lsmod | grep ath12k; then
         echo "- Disabling ath12k wifi kernel module via /etc/modprobe.d/"
         cat <<EOF >/etc/modprobe.d/10-ath12k.conf
 blacklist ath12k
@@ -58,7 +56,13 @@ EOF
 
 configuration() {
 
+    # standard groups
     $DOAS usermod -aG wheel,audio,video,storage,network,kvm $THEUSER
+    # these have probably been added manually but just in case
+    $ADDCMD dbus NetworkManager polkit
+    $DOAS ln -sv /etc/sv/dbus /var/service/
+    $DOAS ln -sv /etc/sv/NetworkManager /var/service/
+
     # https://docs.voidlinux.org/config/services/logging.html
     $ADDCMD socklog-void
     $DOAS ln -sv /etc/sv/socklog-unix /var/service/
@@ -71,40 +75,43 @@ configuration() {
     $DOAS ln -sv /etc/sv/chronyd /var/service/
 
     # https://docs.voidlinux.org/config/session-management.html
+    # ensure not running; elogin will manage power, for now
+    $DOAS rm /var/service/acpid
     $ADDCMD turnstile elogind
-    $DOAS usermod -aG video $THEUSER
-    $DOAS ln -sv /etc/sv/acpid /var/service/
     $DOAS ln -sv /etc/sv/turnstiled /var/service/
-    $DOAS ln -sv /etc/sv/seatd /var/service/
 
     # https://docs.voidlinux.org/config/graphical-session/graphics-drivers/amd.html
     # laptop specific (mine are all Intel igpu)
     if [ -d "/proc/acpi/button/lid" ]; then
-        $ADDCMD intel-media-driver
+        $ADDCMD linux-firmware-intel mesa-dri vulkan-loader mesa-vulkan-intel intel-video-accel
     else
         # Dual GPU system, AMD for Void; NVIDIA for GPU passthrough to Windows VM
         $ADDCMD linux-firmware-amd mesa-dri vulkan-loader mesa-vulkan-radeon mesa-vaapi mesa-vdpau
     fi
 
     # https://docs.voidlinux.org/config/graphical-session/wayland.html
-    # TODO add env vars to startup script
+    $ADDCMD xorg-server-xwayland 
+    # window manager
+    $ADDCMD river foot kanshi wofi Waybar SwayNotificationCenter swaybg swayidle swaylock \
+        wlopm polkit polkit-gnome libnotify nautilus file-roller xdg-user-dirs-gtk xdg-user-dirs \
+        brightnessctl pamixer PAmix
+    xdg-user-dirs-update
+    xdg-user-dirs-update --force
+    # login manager/greeter
+    $ADDCMD greetd tuigreet gnome-keyring
+    # TODO CONFIG plus pam.d config for gnome-keyring
 
     # https://docs.voidlinux.org/config/graphical-session/fonts.html
     fontinstall.sh
 
     # https://docs.voidlinux.org/config/graphical-session/icons.html
     # nothing to do, accept the default gtk adwaita that will get installed as apps are
+    $ADDCMD adwaita-icon-theme
 
     # https://docs.voidlinux.org/config/graphical-session/portals.html
-    $ADDCMD xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-desktop-portal-wlr
-
-    # https://docs.voidlinux.org/config/media/index.html
-    $DOAS usermod -aG audio $THEUSER
-    # https://docs.voidlinux.org/config/media/alsa.html
-    # TODO check default
+    $ADDCMD xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr
 
     # https://docs.voidlinux.org/config/media/pipewire.html
-    $DOAS usermod -aG audio $THEUSER
     $ADDCMD pipewire pulseaudio-utils alsa-pipewire
     $DOAS mkdir -p /etc/pipewire/pipewire.conf.d
     $DOAS ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
@@ -113,84 +120,32 @@ configuration() {
     $DOAS ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
     $DOAS ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
 
-    # window manager
-    $ADDCMD river foot kanshi wofi Waybar SwayNotificationCenter swaybg swayidle swaylock \
-        wlopm polkit polkit-gnome libnotify nautilus file-roller xdg-user-dirs-gtk xdg-user-dirs
-    # login manager/greeter
-    $ADDCMD greetd tuigreet
-    # TODO CONFIG plus pam.d config for gnome-keyring
 }
 
 applications() {
-    $ADDCMD git lazygit delta chezmoi fish-shell htop
+    $ADDCMD git lazygit delta chezmoi htop evolution
+    $ADDCMD helix go nodejs cargo
+}
+
+flatpak(){
     # just in case being run separately
     $ADDCMD flatpak xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr
 
-    flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
     for app in org.gnome.Evolution org.signal.Signal us.zoom.Zoom; do
         echo "Installing $app"
         flatpak install -y $app
     done
-}
-
-userconfig() {
-    xdg-user-dirs-update
-    xdg-user-dirs-update --force
-    chezmoi init git@github.com:mwyvr/dotfiles.git
-    chezmoi apply
-}
-### NOT UPDATED
-#
-# core utils and dotfiles
-# TODO - move back to distrobox
-# for Helix editor and dev
-# $ADDCMD helix go nodejs cargo
-
-# Desktop and laptop get the River window manager (Wayland) and supporting tools; Sway is
-# added to pull in components until this is sorted out
-# applications
-# $ADDCMD evolution
-# flatpak apps
-$ADDCMD flatpak
 # video player
 flatpak remote-add --if-not-exists --user gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
 flatpak install gnome-nightly org.gnome.Showtime.Devel
+}
 
-# install Google Chrome directly
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub >/tmp/chrome-linux_signing_key.pub
-sudo rpm --import /tmp/chrome-linux_signing_key.pub
-sudo zypper addrepo http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
-sudo zypper refresh
-# set refresh on repo
-sudo zypper ms -f Google-Chrome
-$ADDCMD google-chrome-stable
-
-# doing this first avoids pulling in docker for distrobox
-$ADDCMD podman
-$ADDCMD distrobox
-echo "Create a default tumbleweed distrobox with: distrobox enter"
-
-echo 'Configuration completed
-
-Tip: adjust /etc/default/grub:
-GRUB_CMDLINE_LINUX_DEFAULT="plymouth.enable=0 splash=silent resume=/dev/system/swap mitigations=auto security=apparmor"
-And then:
-grub2-mkconfig -o /boot/grub2/grub.cfg
-
-To disable plymouth and return book log to screen
-
-TODO XXX the following packages were not installed - these belong in opensuseway, the sway configuration
-determine what is needed
-adobe-sourcesanspro-fonts adobe-sourceserifpro-fonts adwaita-qt5 bluez bluez-cups brightnessctl clipman dejavu-fonts
-  desktop-file-utils ghostscript-fonts-other google-carlito-fonts google-droid-fonts google-noto-coloremoji-fonts
-  google-noto-sans-fonts google-opensans-fonts jq libadwaitaqt5-1 libcairomm-1_16-1 libell0 libfido2-udev libgiomm-2_68-1
-  libglibmm-2_68-1 libgtkmm-4_0-0 libjq1 libmbim libmbim-glib4 libonig5 libpangomm-2_48-1 libqmi-glib5 libqmi-tools
-  libqrtr-glib0 libQt5X11Extras5 libsigc-3_0-0 mbimcli-bash-completion ModemManager ModemManager-bash-completion
-  MozillaFirefox MozillaFirefox-branding-openSUSE mozilla-openh264 mpris-ctl NetworkManager-bluetooth NetworkManager-wwan
-  openSUSEway pamixer pavucontrol playerctl playerctl-bash-completion qt5ct sbc sqlite3-tcl sway-branding-openSUSE sway-marker
-  SwayNotificationCenter SwayNotificationCenter-bash-completion SwayNotificationCenter-fish-completion tcl usb_modeswitch
-  usb_modeswitch-data wl-clipboard wl-clipboard-bash-completion wl-clipboard-fish-completion wob
-
-see: https://github.com/openSUSE/openSUSEway
-
-'
+# base_hardware
+configuration
+applications
+# flatpak
+echo "
+    Configure greetd + pamd
+    Start greetd & reboot
+"
