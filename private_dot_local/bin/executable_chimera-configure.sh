5c63fdf1b490cb4294690dfce7d0aaa5289aa67f
Mike Watkins <git@mikewatkins.ca>
2025-02-20 10:02:51 -0800

    add chrome back just for widevine (netflix)

--- private_dot_local/bin/executable_chimera-configure.sh
+++ private_dot_local/bin/executable_chimera-configure.sh
@@ -97,7 +97,7 @@ if [ -n "$IS_GUI" ]; then
     # flatpak apps
     $ADDCMD flatpak
     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    for app in org.chromium.Chromium org.signal.Signal us.zoom.Zoom; do
+    for app in com.google.Chrome org.signal.Signal us.zoom.Zoom; do
         echo "Installing $app"
         flatpak install -y $app
     done
